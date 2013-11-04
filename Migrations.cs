244e2781de41b1df9850f6a91031f1e26812e777
Jay Harris <jay@aranasoft.com>
2013-11-04 14:12:58 -0500

    Code cleanup. Compatibility with Orchard v1.7. Update to VS2012

--- Migrations.cs
+++ Migrations.cs
@@ -6,50 +6,41 @@ using Orchard.Data;
 using Orchard.Data.Migration;
 using ShopRunner.Models;
 
-namespace ShopRunner
-{
-    public class Migrations : DataMigrationImpl
-    {
+namespace ShopRunner {
+    public class Migrations : DataMigrationImpl {
         private readonly IRepository<ShopRunnerDivNameRecord> _divNameRepository;
 
-        private readonly IEnumerable<ShopRunnerDivNameRecord> _divNames = new List<ShopRunnerDivNameRecord>
-                                                                          {
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_headerDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_bannerDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_smallBannerDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_productDetailDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_catalogProductDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_catalogProductGridDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_cartProductDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_shippingOptionDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_shippingSummaryDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_cartSummaryDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_duelEligibilityDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_checkoutPageDiv"},
-                                                                              new ShopRunnerDivNameRecord {Name = "sr_checkoutSRItemsPageDiv"}
-                                                                          };
+        private readonly IEnumerable<ShopRunnerDivNameRecord> _divNames = new List<ShopRunnerDivNameRecord> {
+            new ShopRunnerDivNameRecord {Name = "sr_headerDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_bannerDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_smallBannerDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_productDetailDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_catalogProductDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_catalogProductGridDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_cartProductDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_shippingOptionDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_shippingSummaryDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_cartSummaryDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_duelEligibilityDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_checkoutPageDiv"},
+            new ShopRunnerDivNameRecord {Name = "sr_checkoutSRItemsPageDiv"}
+        };
 
-        public Migrations(IRepository<ShopRunnerDivNameRecord> divNameRepository)
-        {
+        public Migrations(IRepository<ShopRunnerDivNameRecord> divNameRepository) {
             _divNameRepository = divNameRepository;
         }
 
-        public int Create()
-        {
-            SchemaBuilder.DropTable("ShopRunnerPartRecord");
-            SchemaBuilder.DropTable("ShopRunnerDivNameRecord");
-            SchemaBuilder.DropTable("ShopRunnerSettingsPartRecord");
-
+        public int Create() {
             SchemaBuilder.CreateTable("ShopRunnerDivNameRecord",
-                                      table => table
-                                                   .Column<int>("Id", column => column.PrimaryKey().Identity())
-                                                   .Column<string>("Name", column => column.WithLength(50))
+                table => table
+                    .Column<int>("Id", column => column.PrimaryKey().Identity())
+                    .Column<string>("Name", column => column.WithLength(50))
                 );
             // Creating table ShopRunnerPartRecord
             SchemaBuilder.CreateTable("ShopRunnerPartRecord",
-                                      table => table
-                                                   .ContentPartRecord()
-                                                   .Column<int>("DivName_Id")
+                table => table
+                    .ContentPartRecord()
+                    .Column<int>("DivName_Id")
                 );
 
             ContentDefinitionManager.AlterPartDefinition(
@@ -58,24 +49,21 @@ namespace ShopRunner
 
             // Creating table ShopRunnerSettingsPartRecord
             SchemaBuilder.CreateTable("ShopRunnerSettingsPartRecord",
-                                      table => table
-                                                   .ContentPartRecord()
-                                                   .Column<string>("RetailerId")
-                                                   .Column<string>("LoginValidationUrl")
-                                                   .Column<string>("EnvironmentId", col => col.WithDefault(ShopRunnerEnvironmentMode.Development.ToString()))
+                table => table
+                    .ContentPartRecord()
+                    .Column<string>("RetailerId")
+                    .Column<string>("LoginValidationUrl")
+                    .Column<string>("EnvironmentId", col => col.WithDefault(ShopRunnerEnvironmentMode.Development.ToString()))
                 );
 
             return 1;
         }
 
-        public int UpdateFrom1()
-        {
-            if (_divNameRepository == null)
-            {
+        public int UpdateFrom1() {
+            if (_divNameRepository == null) {
                 throw new InvalidOperationException("Couldn't find DIV Name repository.");
             }
-            foreach (ShopRunnerDivNameRecord divName in _divNames)
-            {
+            foreach (var divName in _divNames) {
                 _divNameRepository.Create(divName);
             }
 
